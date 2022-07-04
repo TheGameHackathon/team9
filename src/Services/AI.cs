8c8113cf6ef3b3dfcec070c406bb083a2603f0eb
artemworld <artyom.skripkin@list.ru>
2022-07-04 14:36:14 +0500

    Update AI.cs

--- src/Services/AI.cs
+++ src/Services/AI.cs
@@ -1,66 +1,93 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using thegame.Models;
 
 namespace thegame.Services;
 
 public class AI
 {
-    public void MakeStep(GameDto gameDto)
+
+    /*public GameDto MakeStep(GameDto gameDto, string newColor)
     {
-        Dictionary<string, int> counter;
+        string chooseColor = PreProcess(gameDto);
+        return;
+    }*/
+
+    private string PreProcess(GameDto gameDto)
+    {
+        Dictionary<string, int> counter = new Dictionary<string, int>();
         DSU dsu = new DSU();
+        //GameChanger gameChanger = new GameChanger();
         CellDto startPos = null;
         foreach (var cell in gameDto.Cells)
         {
-            dsu.make_set(cell);
             if (cell.Pos.X == 0 && cell.Pos.Y == 0)
             {
 	            startPos = cell;
             }
         }
-        
+
+        var firstComponent = new HashSet<CellDto>(); 
+        dsu.GetComponent(gameDto, startPos, firstComponent);
+        HashSet<CellDto> used = new HashSet<CellDto>(firstComponent);
+
+        foreach (var start in firstComponent)
+        {
+            counter[start.Type] += dsu.GetComponent(gameDto, start, used);
+        }
+
+        int maxVal = -1;
+        string resultColor = null;
+        foreach (var data in counter)
+        {
+            if (maxVal < data.Value)
+            {
+                maxVal = data.Value;
+                resultColor = data.Key;
+            }
+        }
+
+        return resultColor;
     }
 }
 
 public class DSU
 {
-	private Dictionary<CellDto, int> depth;
-	private Dictionary<CellDto, CellDto> parents;
+    public int GetComponent(GameDto gameDto, CellDto startPos, HashSet<CellDto> used)
+        {
+            //HashSet<CellDto> used = new HashSet<CellDto>();
+            Queue<CellDto> queue = new Queue<CellDto>();
+            queue.Enqueue(startPos);
+            int elementsCount = 0;
+            while (queue.Count != 0)
+            {
+                var currentCell = queue.Dequeue();
+                used.Add(currentCell);
+                elementsCount++;
+                var sides = GetSides(currentCell, gameDto);
+                foreach (var toE in sides)
+                {
+                    if (!used.Contains(toE))
+                        queue.Enqueue(toE);
+                }
+            }
 
-	public DSU()
-	{
-		depth = new Dictionary<CellDto, int>();
-		parents = new Dictionary<CellDto, CellDto>();
-	}
-
-	public void make_set(CellDto x)
-	{
-		depth[x] = 1;
-		parents[x] = x;
-	}
-	
-	public CellDto find_set(CellDto x)
-	{
-		if (x == parents[x])
-			return x;
-		return parents[x] = find_set(parents[x]);
-	}
-	
-	void union_sets(CellDto a, CellDto b)
-	{
-		CellDto root1 = find_set(a);
-		CellDto root2 = find_set(b);
-		if (root1 == root2)
-			return;
-		if (depth[root1] >= depth[root2])
-		{
-			parents[root2] = root1;
-			depth[root1] += depth[root2];
-		}
-		else
-		{
-			parents[root1] = root2;
-			depth[root2] += depth[root1];
-		}
-	}
+            return elementsCount - 1;
+        }
+        
+        public List<CellDto> GetSides(CellDto cell, GameDto gameDto)
+        {
+            List<CellDto> sides = new List<CellDto>();
+    
+            foreach (var nCell in gameDto.Cells)
+            {
+                if ((Math.Abs(nCell.Pos.X - cell.Pos.X) == 1 && Math.Abs(nCell.Pos.Y - cell.Pos.Y) == 0
+                     || Math.Abs(nCell.Pos.X - cell.Pos.X) == 0 && Math.Abs(nCell.Pos.Y - cell.Pos.Y) == 1)
+                    && nCell.Type == cell.Type)
+                {
+                    sides.Add(nCell);
+                }
+            }
+            return sides;
+        }
 }
